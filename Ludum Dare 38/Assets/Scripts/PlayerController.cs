b961994d6d33ab3deab648b7debef59f7c8a0fee
Brandon Smith <brandon.smith.945@gmail.com>
2017-04-21 22:58:01 -0500

    Movement and turning works

--- Ludum Dare 38/Assets/Scripts/PlayerController.cs
+++ Ludum Dare 38/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,18 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
     this.GetComponent<Rigidbody>().centerOfMass = this.GetComponent<Transform>().position;
-
 	}
 
 	// Update is called once per frame
 	void Update () {
-    this.GetComponent<Rigidbody>().AddTorque(new Vector3(100, 0, 0));
+    this.GetComponent<Rigidbody>().rotation = Quaternion.AngleAxis(1, this.transform.right) * this.GetComponent<Rigidbody>().rotation;
 
-    if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+    if(Input.GetKey(KeyCode.LeftArrow)) {
+      this.GetComponent<Rigidbody>().rotation = Quaternion.AngleAxis(-1f, this.transform.up) * this.GetComponent<Rigidbody>().rotation;
+    } else if(Input.GetKey(KeyCode.RightArrow)) {
+      this.GetComponent<Rigidbody>().rotation = Quaternion.AngleAxis(1f, this.transform.up) * this.GetComponent<Rigidbody>().rotation;
+    } else {
+      //torque.z = -0.1f * this.GetComponent<Rigidbody>().angularVelocity.z;
     }
-    if(Input.GetKeyDown(KeyCode.RightArrow)) {
-    }
-
 	}
 }
