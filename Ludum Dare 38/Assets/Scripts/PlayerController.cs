1da7cb9d64e93bf6b4d79ded757442c4e65d0e15
Brandon Smith <brandon.smith.945@gmail.com>
2017-04-21 22:02:27 -0500

    Added first scene and got teh moooving

--- Ludum Dare 38/Assets/Scripts/PlayerController.cs
+++ Ludum Dare 38/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+    this.GetComponent<Rigidbody>().centerOfMass = this.GetComponent<Transform>().position;
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+    this.GetComponent<Rigidbody>().AddTorque(new Vector3(100, 0, 0));
+
+    if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+    }
+    if(Input.GetKeyDown(KeyCode.RightArrow)) {
+    }
+
 	}
 }
